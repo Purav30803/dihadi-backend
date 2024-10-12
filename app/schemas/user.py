6b33fc9fb0016ca5c6f7d8b395e8ad0c034d20d1
Purav30803 <shah392@uwindsor.ca>
2024-10-11 22:09:27 -0400

    Refactor user schema to use string type for email and skills

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -3,13 +3,13 @@ from typing import List, Optional
 
 class UserCreate(BaseModel):
     name: str
-    email: EmailStr
+    email: str
     phone: str
     password: str
     age: int
     # nationality: str
     is_student: bool
-    skills: List[str]
+    skills: str
     id_proof: str
     # company_name: Optional[str] = None
     location: Optional[str] = None
@@ -17,12 +17,12 @@ class UserCreate(BaseModel):
 class UserResponse(BaseModel):
     user_id: str
     name: str
-    email: EmailStr
+    email: str
     phone: str
     age: int
     # nationality: str
     is_student: bool
-    skills: List[str]
+    skills: str
     id_proof: str
     # company_name: Optional[str] = None
     location: Optional[str] = None
